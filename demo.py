622c14eecde4126a49cc52b5346cdc3f227b022a
Shivam Sherkar <shivam23sherkar@gmail.com>
2021-01-06 18:40:42 +0530

    changes

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-print("data version control demo")
+import dvc.api
+
+resource_url = dvc.api.get_url(
+    'get-started/data.xml',
+    repo='https://github.com/iterative/dataset-registry'
+)
+
+print(resource_url)
