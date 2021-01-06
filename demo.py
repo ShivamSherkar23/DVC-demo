66281b3618b0e34873fada17b1e04e438f29ce08
Shivam Sherkar <shivam23sherkar@gmail.com>
2021-01-06 18:43:54 +0530

    modified file

--- demo.py
+++ demo.py
@@ -4,5 +4,5 @@ resource_url = dvc.api.get_url(
     'get-started/data.xml',
     repo='https://github.com/iterative/dataset-registry'
 )
-
+print("output from the get url method is ")
 print(resource_url)
